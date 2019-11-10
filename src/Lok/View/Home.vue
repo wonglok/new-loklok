6ffc00b352c4399100dd91c81943ba1de5269273
wonglok <yellowhappy831@gmail.com>
2019-11-10 11:33:54 +0800

    github backup

--- src/Lok/View/Home.vue
+++ src/Lok/View/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div class=" text-teal-500 font-serif font-hairline text-xl p-5">
-      <a class="block" href="/lok">For Lok</a>
-      <a class="block" href="/bear">For Voyage Bear</a>
-      <a class="block" href="/bryant-and-jenni-2019">For B&J</a>
-      <a class="block" href="/words">Current Experiment: Words</a>
+      <a class="block" href="/lok">For Lok - lok</a>
+      <a class="block" href="/bear">For Voyage Bear - lok</a>
+      <a class="block" href="/bryant-and-jenni-2019">For B&J - lok</a>
+      <a class="block" href="/words">Caligraphy - lok & giraffle</a>
     </div>
   </div>
 </template>
